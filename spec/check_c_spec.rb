398cd38e0c121e8845feb143089c3931e69e2776
OwlinOhana <47669603+OwlinOhana@users.noreply.github.com>
2021-11-22 15:53:38 +0700

    Update check_c_spec.rb

--- spec/check_c_spec.rb
+++ spec/check_c_spec.rb
@@ -7,33 +7,33 @@ describe ValeraInit do
   let(:mana) { 0 }
   let(:happiness) { 0 }  
   describe '#dead?' do
-    subject { character.dead? }
-    context 'when the character health < 0' do
+    subject { valera.dead? }
+    context 'when the valera health < 0' do
       let(:health) { -5 }
       it { is_expected.to be true }
     end
 
-    context 'when the character health > 0' do
+    context 'when the valera health > 0' do
       let(:health) { 50 }
       it { is_expected.to be false }
     end
 
-    context 'when the character happiness > -10' do
+    context 'when the valera happiness > -10' do
       let(:happiness) { 5 }
       it { is_expected.to be true }
     end
 
-    context 'when the character happiness < -10' do
+    context 'when the valera happiness < -10' do
       let(:happiness) { -15 }
       it { is_expected.to be true }
     end
   end
 
   describe '#mana=' do
-    subject { character.mana }
+    subject { valera.mana }
     let(:mana) { 130 }
 
     it { expect(subject).to eq(100) }
-    it { expect(character.health).to eq(0) }
+    it { expect(valera.health).to eq(0) }
   end
 end
